931389321b3858bd2e16c283d30c096b0f115a51
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2021-01-05 07:16:35 +0000

    General improvements to this file. * Replaced $1/$2/$3 with INFILE, OUTFILE and CHECKNAMEDCONF respectively. * Replaced double quotes with single quotes around grep regex. * Ran shellcheck over the file, and made some recommended replacements. * Moved logic around the order of running `named-checkconf`.

--- files/validate-bind-db.sh
+++ files/validate-bind-db.sh
@@ -1,40 +1,43 @@
 #! /bin/bash
-if [ ! -e "$1" ]
+INFILE="$1"
+OUTFILE="$2"
+CHECKNAMEDCONF="$3"
+
+if [ ! -e "$INFILE" ]
 then
   echo "Missing source file."
   exit 2
 fi
-if [ ! -e "$2" ]
+if [ ! -e "$OUTFILE" ]
 then
-  echo "Missing destination file. Copying $1 to $2."
-  mv "$1" "$2"
+  echo "Missing destination file. Copying $INFILE to $OUTFILE."
+  mv "$INFILE" "$OUTFILE"
   exit 1
 fi
-source=$(grep -vE "\s+[0-9]{10}\s+" "$1" | md5sum - | cut -d\  -f1)
-destination=$(grep -vE "\s+[0-9]{10}\s+" "$2" | md5sum - | cut -d\  -f1)
+source=$(grep -vE '\s+[0-9]{10}\s+' "$INFILE" | md5sum - | cut -d\  -f1)
+destination=$(grep -vE '\s+[0-9]{10}\s+' "$OUTFILE" | md5sum - | cut -d\  -f1)
 if [ "$source" == "$destination" ]
 then
   echo "No differences."
-  rm "$1"
+  rm "$INFILE"
   exit 0
 else
-  echo "Differences. Replacing $1 ($source) over $2 ($destination)."
-  grep -vE "\s+[0-9]{10}\s+" "$1" > /tmp/bind_diff_src
-  grep -vE "\s+[0-9]{10}\s+" "$2" > /tmp/bind_diff_dst
+  echo "Differences. Replacing $INFILE ($source) over $OUTFILE ($destination)."
+  grep -vE '\s+[0-9]{10}\s+' "$INFILE" > /tmp/bind_diff_src
+  grep -vE '\s+[0-9]{10}\s+' "$OUTFILE" > /tmp/bind_diff_dst
   diff /tmp/bind_diff_src /tmp/bind_diff_dst
   rm /tmp/bind_diff_src /tmp/bind_diff_dst
-  if [ -n "$3" ]
+  if [ -n "$CHECKNAMEDCONF" ]
   then
-    cp "$2" "$2~"
-    /usr/sbin/named-checkconf -z "/etc/bind/named.conf" 2>&1 > "/tmp/named-checkconf.out"
-    if [ $? -gt 0 ]
+    cp "$OUTFILE" "$OUTFILE~"
+    if /usr/sbin/named-checkconf -z "/etc/bind/named.conf" >"/tmp/named-checkconf.out" 2>&1
     then
       cat "/tmp/named-checkconf.out"
-      rm "$1" "$2~" "/tmp/named-checkconf.out"
+      rm "$INFILE" "$OUTFILE~" "/tmp/named-checkconf.out"
       exit 3
     fi
+    rm -f "/tmp/named-checkconf.out"
   fi
-  mv "$1" "$2"
-  rm -f "/tmp/named-checkconf.out"
+  mv "$INFILE" "$OUTFILE"
   exit 1
 fi
